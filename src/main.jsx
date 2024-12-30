ba21e3251a7e2d1e12c664ff3217c328a6f54ef5
workvivek <workvivek603@gmail.com>
2024-12-30 17:11:33 +0530

    path of index css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
+import '../index.css'
 import App from './App.jsx'
 import { AuthProvider } from './store/auth.jsx'
 import { ToastContainer } from 'react-toastify'
