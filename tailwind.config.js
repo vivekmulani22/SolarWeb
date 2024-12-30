7ebf32a7628ad5a7184aa4b07ca97a756b2de66d
workvivek <workvivek603@gmail.com>
2024-12-30 17:31:00 +0530

    tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,38 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+module.exports = {
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./public/index.html", // Include your HTML files if necessary
   ],
+  darkMode: false,
   theme: {
+    extend: {
+      transitionDuration: ["hover", "focus"],
+      fontSize: {
+        sm: ["15px"],
+        base: ["16px", "24px"],
+        lg: ["25px", "28px"],
+        xl: "40px",
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
- 
+  corePlugins: {
+    container: false,
+  },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          maxWidth: "100%",
+          "@screen sm": { maxWidth: "600px" },
+          "@screen md": { maxWidth: "765px" },
+          "@screen lg": { maxWidth: "1320px" },
+          "@screen xl": { maxWidth: "1320px" },
+        },
+      });
+    },
+  ],
 };
